dd307414274918718db9dfb33af58d1a9394a0e8
Adam Tulloss <artulloss@gmail.com>
2024-02-07 22:27:34 -0500

    Started project

---
+++ Calculator/Calculator/StaticProperties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class StaticProperties
+    {
+        public const int BUTTON_SIZE = 100; // Size as an integer
+        public static readonly int CORNER_RADIUS = BUTTON_SIZE / 2; // Computed at runtime
+
+        public static readonly Color COLOR_PRIMARY = Color.FromHex("#efa24b");
+        public static readonly Color COLOR_SECONDARY = Color.FromHex("#555555");
+        public static readonly Color COLOR_TERTIARY = Color.FromHex("#aaaaaa");
+
+        public static readonly Color COLOR_BLACK = Color.Black;
+        public static readonly Color COLOR_WHITE = Color.FromHex("#f5f5f5");
+    }
+}
