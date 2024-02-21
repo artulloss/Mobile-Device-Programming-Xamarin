afab24790f3ed1b2275cd43f4100b5273526eace
Adam Tulloss <artulloss@gmail.com>
2024-02-20 22:00:07 -0500

    Reading data from the sensor with our MainPage in scope with delegate

---
+++ Sensor/Sensor/CompassSensor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace Sensor
+{
+    public class CompassSensor
+    {
+        // Set speed delay for monitoring changes.
+        readonly SensorSpeed speed = SensorSpeed.UI;
+
+        private bool _running = false;
+
+        public delegate void CompassReadingHandler(object sender, CompassChangedEventArgs e);
+
+        private CompassReadingHandler _onReadingChanged;
+
+        public CompassSensor(CompassReadingHandler onReadingChanged)
+        {
+            // Register for reading changes, be sure to unsubscribe when finished
+            Compass.ReadingChanged += Compass_ReadingChanged;
+            _onReadingChanged = onReadingChanged;
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            _onReadingChanged.Invoke(sender, e);
+        }
+
+        public void ToggleCompass()
+        {
+            _running = !_running;
+            SetCompass(_running);
+        }
+
+        public void SetCompass(bool running = false)
+        {
+            try
+            {
+            if(running && !Compass.IsMonitoring)
+            {
+                Compass.Start(speed);
+                _running = true;
+            } else if (!running && Compass.IsMonitoring)
+            {
+                Compass.Stop();
+                _running = false;
+            }
+            } catch (FeatureNotSupportedException e)
+            {
+
+            } catch (FeatureNotEnabledException e)
+            {
+
+            } catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}
+
---
+++ Sensor/Sensor/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Sensor
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly CompassSensor _sensor;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _sensor = new CompassSensor(On_Compass_ReadingChanged);
+        }
+
+        void Toggle_Compass(System.Object sender, System.EventArgs e)
+        {
+            _sensor.ToggleCompass();
+        }
+
+
+        void On_Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            var data = e.Reading;
+            CompassData.Text = $"{data.HeadingMagneticNorth} degrees";
+        }
+
+    }
+}
+
